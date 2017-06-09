e46a4f2420405a180bfae85377003f047bc22ac6
Johan 't Hart <johan@jdm.nl>
2017-06-09 13:19:01 +0200

    WIP: Add restCall.ts

--- index.ts
+++ index.ts
@@ -1,7 +1,28 @@
 import { Observable } from 'rxjs/rx';
 import { log, logging } from './logging'
-import './patternMatch'
+//import './patternMatch'
+//import './restCall'
 
 
 //console.log('');
 
+Observable
+    .range(1,14)
+    .map(x => x*x)
+    .subscribe(log);
+
+function factorize(n: number):number[]{
+    let result: number[] = [];
+    for (let x = 2; x < Math.sqrt(n); ++x) {
+        if ( n % x === 0) {
+            result.push(x);
+            result.concat(factorize(n/x));
+        }
+    }
+    return result;
+}
+
+[91,137,150,154]
+//    .map(x => Math.sqrt(x))
+    .map(factorize)
+    .forEach(log);
---
+++ restCall.ts
@@ -0,0 +1,5 @@
+import { Observable } from 'rxjs/rx';
+import { log } from './logging';
+//import { Request } from 'request';
+
+//Request('www.google.com');
