57e00eeb3650b9d93f3fc4cbbb5ad1c7f2948f1e
Johan 't Hart <johan@jdm.nl>
2017-06-24 17:12:25 +0200

    Make some test cases

--- index.ts
+++ index.ts
@@ -1,13 +1,43 @@
 import { Observable } from 'rxjs/rx';
-import { log, logging } from './logging';
-import './factorize';
+import { log } from './logging';
+//import './factorize';
 //import './patternMatch'
 //import './restCall'
 
+const expect = exp => value => {
+    if (exp != value) {
+        console.error(`Expected value to be ${exp}, but it was found to be ${value}`);
+        return false;
+    }
+    return true;
+}
 
-//console.log('');
+function getFirstNr(obs: Observable<number>):Promise<number> {
+    return Promise.resolve(1);
+}
 
-Observable
-    .range(1,14)
-    .map(x => x*x)
-    .subscribe(log);
+
+function testGetFirstNr1() : Promise<boolean> {
+    return getFirstNr(Observable.interval(1000))
+        .then(expect(0));
+}
+
+function testGetFirstNr2() : Promise<boolean> {
+    return getFirstNr(Observable.range(3,10))
+        .then(expect(3));
+}
+
+const tests = [
+    testGetFirstNr1,
+    testGetFirstNr2,
+];
+
+function runTests() {
+    Promise.all(tests.map(test => test()))
+        .then(results => results.reduce((p,c) => c ? p : p + 1, 0))
+        .then(failures => failures == 0 ?
+            console.log("All tests succeeded") :
+            console.error(`${failures} tests failed`));
+}
+
+runTests();
