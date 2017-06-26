46c46f36b82d5b73acac2b0c060c7815f5c00d66
Johan 't Hart <johan@jdm.nl>
2017-06-26 11:59:37 +0200

    Add getFirstNr() naive implementation

--- getFirstNr.ts
+++ getFirstNr.ts
@@ -1,5 +1,22 @@
 import { Observable } from 'rxjs/rx';
 
-export function getFirstNr(obs: Observable<number>):Promise<number> {
+export function getFirstNr__(obs: Observable<number>):Promise<number> {
     return Promise.resolve(1);
+}
+
+export function getFirstNr(obs: Observable<number>):Promise<number> {
+    return obs.take(1).toPromise()
+        .then(i => { if (i === undefined) { throw new Error('No value received'); } return i; });
+}
+
+export function getFirstNr_(obs: Observable<number>):Promise<number> {
+    return new Promise((resolve, reject) => {
+        const subscription = obs.subscribe(
+            value => {
+                resolve(value);
+                subscription.unsubscribe();
+            },
+            error => reject(error),
+            () => reject(new Error('Completed before getting a value')))
+    });
 }
--- tests.ts
+++ tests.ts
@@ -4,7 +4,7 @@ import { log } from './logging';
 import { getFirstNr } from './getFirstNr';
 
 const expect = exp => value => {
-    if (exp != value) {
+    if (exp !== value) {
         console.error(`Expected value to be ${exp}, but it was found to be ${value}`);
         return false;
     }
@@ -21,17 +21,34 @@ function testGetFirstNr2() : Promise<boolean> {
         .then(expect(3));
 }
 
+function testExpectException(obs: Observable<number>): Promise<boolean> {
+    return getFirstNr(obs)
+        .then(v => { log(`Not expecting a value but got ${v}`); return false; })
+        .catch(_ => true); // Expect failure
+}
+
+function testGetFirstNr3(): Promise<boolean> {
+    return testExpectException(Observable.empty());
+}
+
+function testGetFirstNr4(): Promise<boolean> {
+    return testExpectException(Observable.throw(new Error('Just an error')));
+}
+
 const tests = [
     testGetFirstNr1,
     testGetFirstNr2,
+    testGetFirstNr3,
+    testGetFirstNr4
 ];
 
 function runTests() {
     Promise.all(tests.map(test => test()))
         .then(results => results.reduce((p,c) => c ? p : p + 1, 0))
-        .then(failures => failures == 0 ?
+        .then(failures => failures === 0 ?
             console.log("All tests succeeded") :
-            console.error(`${failures} tests failed`));
+            console.error(`${failures} tests failed`))
+        .catch(e => log(`Things went wrong. ${e}`));
 }
 
 runTests();
